37815dbd1d4d7f9c6ab7da4ad97a8c56323b3af8
jawaanlewis-star <jawaan.lewis@gmail.com>
2026-01-18 22:50:19 -0600

    Update amazon-linux-apache.sh

--- user-data/amazon-linux-apache.sh
+++ user-data/amazon-linux-apache.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+dnf -y update
+dnf -y install httpd
+systemctl enable httpd
+systemctl start httpd
+echo "<h1>Jawaan's EC2 Apache Server</h1><p>Deployed with EC2 user-data</p>" > /var/www/html/index.html
+
 [    6.147426] cloud-init[1519]: Cloud-init v. 22.2.2 running 'init' at Mon, 19 Jan 2026 03:50:40 +0000. Up 6.12 seconds.
 [    6.218068] cloud-init[1519]: ci-info: ++++++++++++++++++++++++++++++++++++++Net device info+++++++++++++++++++++++++++++++++++++++
 [    6.220203] cloud-init[1519]: ci-info: +--------+------+-----------------------------+---------------+--------+-------------------+
